10b98dafb351ba4305fcd45c72cf90b3a1b762ca
Aantarik <131798510+Aantarik@users.noreply.github.com>
2023-04-26 16:27:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const bodyParser = require('body-parser');
 //   // perform actions on the collection object
 //   client.close();
 // });
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(express.static(__dirname+'/dist/MyApp'));
 app.use(express.json({extended: false})); 
